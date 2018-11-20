bd18f5171ceaa17b4dc175b63ea4f8f40e4bb010
JEFFERSON\jeffe_000 <jeffersonrivera1402@gmail.com>
2018-11-20 06:07:26 -0500

    Simple repo
    Version 1

---
+++ class_lib/main_fotter.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+          UNMSM - FISI.
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2018 Grupo de Calidad de Software | <a href=''></a></strong> | Todos los derechos reservados XD .
+      </footer>
